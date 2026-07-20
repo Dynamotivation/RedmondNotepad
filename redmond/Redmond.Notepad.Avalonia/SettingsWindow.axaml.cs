da20a12f95106d28dd9738a6259c00c95e7cb784
Dynamotivation <github@dynamotivation.com>
2026-07-20 09:11:15 +0200

    Apply system material to Notepad settings

--- redmond/Redmond.Notepad.Avalonia/SettingsWindow.axaml.cs
+++ redmond/Redmond.Notepad.Avalonia/SettingsWindow.axaml.cs
@@ -6,6 +6,7 @@ namespace Redmond.Notepad.Avalonia;
 public partial class SettingsWindow : Window
 {
     private const double WideLayoutThreshold = 900;
+    private readonly WindowAppearanceController _appearanceController;
 
     public SettingsWindow()
         : this(new WindowAppearanceOptions())
@@ -15,13 +16,28 @@ public partial class SettingsWindow : Window
     public SettingsWindow(WindowAppearanceOptions appearance)
     {
         InitializeComponent();
+        _appearanceController = WindowAppearanceController.Attach(
+            this,
+            options: appearance,
+            manageDecorations: false);
         WindowAppearancePanel.SetOptions(appearance);
-        WindowAppearancePanel.AppearanceChanged += (_, options) => AppearanceChanged?.Invoke(this, options);
+        WindowAppearancePanel.AppearanceChanged += OnAppearanceChanged;
+        UpdateMaterialSurface(appearance);
         SizeChanged += (_, _) => UpdateResponsiveLayout(Bounds.Width);
         Opened += (_, _) => UpdateResponsiveLayout(Bounds.Width);
     }
 
     public event EventHandler<WindowAppearanceOptions>? AppearanceChanged;
+
+    private void OnAppearanceChanged(object? sender, WindowAppearanceOptions options)
+    {
+        _appearanceController.Apply(options);
+        UpdateMaterialSurface(options);
+        AppearanceChanged?.Invoke(this, options);
+    }
+
+    private void UpdateMaterialSurface(WindowAppearanceOptions options) =>
+        SettingsSurface.Classes.Set("noBackdrop", !options.UseSystemBackdrop);
 
     private void UpdateResponsiveLayout(double width)
     {
